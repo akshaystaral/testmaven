79a2dc14b572e937003780f47dc0700580d7be01
akshaystaral <akshaystaral@gmail.com>
2021-04-14 12:15:47 +0530

    Maven Project First Commit:

---
+++ src/test/java/udemymaven/AppiumTest.java
@@ -0,0 +1,24 @@
+package udemymaven;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeApp()
+	{
+		System.out.println("NativeApp");
+	}
+	
+	@Test
+	public void MobileLogin()
+	{
+		System.out.println("MobileLogin");
+	}
+	
+	@Test
+	public void IosApp()
+	{
+		System.out.println("IosApp");
+	}
+}
---
+++ src/test/java/udemymaven/RESTAPITest.java
@@ -0,0 +1,18 @@
+package udemymaven;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void afterJira()
+	{
+		System.out.println("afterJira");
+	}
+	
+	@Test
+	public void deleteCache()
+	{
+		System.out.println("deleteCache");
+	}
+}
